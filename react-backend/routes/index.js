e6acaccd670a9f2bfe9789151d0e78dcb535d577
Jenny Knuth <inkboco@gmail.com>
2018-04-21 09:40:48 -0600

    set up dynamic url with location

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -3,15 +3,38 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
-/* GET api */
-router.get('/api/weather', function(req, res, next) {
-  var baseUrl = 'https://api.darksky.net';
-  const forecastUrl = baseUrl + '/forecast/' + process.env.API_KEY + '/37.8267,-122.4233';
-  console.log('forecastURL', forecastUrl);
-  // res.render('index', { title: 'Express' });
-  axios.get(forecastUrl)
-    .then(response => res.json(response.data))
-    .catch(error => error);
+/* GET api/weather */
+router.get('/api/weather/:lat/:lon', function(req, res, next) {
+  // var baseUrl = 'https://api.darksky.net';
+  // const forecastUrl = baseUrl + '/forecast/' + process.env.API_KEY + '/' + req.params.lat + ',' + req.params.lon;
+  // console.log('forecastUrl', forecastUrl);
+  // axios.get(forecastUrl)
+  //   .then(response => res.json(response.data))
+  //   .catch(error => error);
+  res.json({
+    "currently": {
+      "time": 1524324949,
+      "summary": "Light Rain",
+      "icon": "rain",
+      "nearestStormDistance": 0,
+      "precipIntensity": 0.013,
+      "precipIntensityError": 0.001,
+      "precipProbability": 1,
+      "precipType": "rain",
+      "temperature": 35.53,
+      "apparentTemperature": 35.53,
+      "dewPoint": 32.4,
+      "humidity": 0.88,
+      "pressure": 1022.5,
+      "windSpeed": 2.7,
+      "windGust": 4,
+      "windBearing": 23,
+      "cloudCover": 0.78,
+      "uvIndex": 2,
+      "visibility": 5.28,
+      "ozone": 367.72
+    },
+  });
 });
 
 module.exports = router;
--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,16 +7,18 @@ import Widget from './Widget/Widget';
 class App extends React.Component {
   state = {
     currently: {},
+    location: { lat: 40.0164, lon: -105.2858 },
   }
 
-  async componentDidMount() {
-    fetch('/api/weather')
+  componentDidMount() {
+    fetch(`/api/weather/${this.state.location.lat}/${this.state.location.lon}`)
       .then(res => res.json())
       .then(data => this.setState({ currently: data.currently }));
   }
 
   render() {
     console.log('currently', this.state.currently);
+    console.log('location', this.state.location);
     return (
       <div className={styles.app}>
         <Widget>{this.state.currently}</Widget>
